bd808420e43831b9e2a3abc7c21d5ebbbaf12810
Shresthadeepika <deepikashrestha82@gmail.com>
2020-04-15 14:36:17 +0545

    User middleware added

--- fyp/app/Http/Kernel.php
+++ fyp/app/Http/Kernel.php
@@ -65,6 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkAdminMiddleware' => \App\Http\Middleware\CheckAdminMiddleware::class,
+        'checkUserMiddleware' => \App\Http\Middleware\CheckUserMiddleware::class,
     ];
 
     /**
---
+++ fyp/app/Http/Middleware/CheckUserMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckUserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check() && auth()->user()->is_admin == 0)
+        {
+            return $next($request);
+        }
+            return redirect('auth.login')->with('error','Please login to access this page.');
+    }
+}
